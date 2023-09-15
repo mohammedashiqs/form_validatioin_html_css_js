9649233446b2a2ecdf3227d7d35fd79eda06c4b7
mohammedashiqs <mrashiq.tvm@gmail.com>
2023-09-15 18:09:09 +0530

    task completed

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,45 @@
-let fullName_flag = false;
-let emailId_flag = false;
-let mobNum_flag = false;
-let city_flag = false;
-let gender_flag = false;
-
-let fullName;
-let emailId;
-let mobNum;
-let city;
-let male;
-let female;
-let languages = []
-
-
-function validateName() {
 
 
 
+function form_handler(e) {
+    e.preventDefault();
+
+
+    let fullName_flag = false;
+    let emailId_flag = false;
+    let mobNum_flag = false;
+    let city_flag = false;
+    let gender_flag = false;
+
+    let fullName;
+    let emailId;
+    let mobNum;
+    let city;
+    let male;
+    let female;
+    let languages = []
+
+
+    /* Get All the input values */
     fullName = document.getElementById("fullName").value;
 
+    emailId = document.getElementById("emailId").value;
+    mobNum = document.getElementById("mobNum").value;
+    city = document.getElementById("city").value;
+    male = document.getElementById("male").checked ? document.getElementById("male").value : ""
+    female = document.getElementById("female").checked ? document.getElementById("female").value : ""
+
+
+    document.getElementById('language1').checked ? languages.push(document.getElementById('language1').value) : ""
+    document.getElementById('language2').checked ? languages.push(document.getElementById('language2').value) : ""
+    document.getElementById('language3').checked ? languages.push(document.getElementById('language3').value) : ""
+    document.getElementById('language4').checked ? languages.push(document.getElementById('language4').value) : ""
+    document.getElementById('language5').checked ? languages.push(document.getElementById('language5').value) : ""
+
+
+
+
+    /* FullName Validation */
     let fullName_regex = /^[A-Za-z]+(\s[A-Za-z]+)*$/
 
     if (fullName.length === 0) {
@@ -40,21 +61,19 @@ function validateName() {
 
         }
     }
-}
 
 
-function validateEmail() {
-
-    /* let emailId_flag = false */
-
-    emailId = document.getElementById("emailId").value;
 
 
+
+
+
+
+    /* Email Validation */
     let email_regex = /^\S+@\S+\.\S+$/; //Example: admin@gmail.com
     if (emailId.length === 0) {
         document.getElementById("emailIdError").innerText = "Email field cannot be empty"
         document.getElementById("emailIdError").style.color = "red"
-        console.log("h")
     } else {
         if (email_regex.test(emailId) === false) {
             document.getElementById("emailIdError").innerText = "Invalid email"
@@ -68,17 +87,10 @@ function validateEmail() {
     }
 
 
-}
 
 
 
-
-
-function validateMobNum() {
-
-    mobNum = document.getElementById("mobNum").value;
-
-
+    /* Mobile Number Validation */
     let mobNum_regex = /^[6-9]\d{9}$/; //ex
     if (mobNum.length === 0) {
         document.getElementById("mobNumError").innerText = "Number field cannot be empty"
@@ -95,13 +107,9 @@ function validateMobNum() {
     }
 
 
-}
 
 
-function validateCity() {
-    city = document.getElementById("city").value;
-
-
+    /* City Validation */
     if (city == "") {
         document.getElementById("cityError").innerHTML = "Please select the city name"
         document.getElementById("cityError").style.color = "red"
@@ -111,75 +119,53 @@ function validateCity() {
         document.getElementById("cityError").style.color = "green"
         city_flag = true
     }
-}
 
 
 
-function validateGender() {
-    male = document.getElementById("male").value;
-    female = document.getElementById("female").value;
+    /* Gender Validation */
 
-    console.log(male, female)
-    console.log("hi")
 
-    if (male != "" && female != "") {
+    if (male == "male" || female == "female") {
         document.getElementById("genderError").innerHTML = "selected"
         document.getElementById("genderError").style.color = "green"
         gender_flag = true
+
+    } else {
+        document.getElementById("genderError").innerHTML = "Please select the Gender"
+        document.getElementById("genderError").style.color = "red"
     }
-}
 
 
 
 
-let pc_output = document.getElementById("user_data_pc")
-let tab_and_mob_view = document.getElementById("tab_and_mob_view")
-
-
-
-function submitFrom(e) {
-    e.preventDefault();
-
-    document.getElementById('language1').checked ? languages.push(document.getElementById('language1').value) : ""
-    document.getElementById('language2').checked ? languages.push(document.getElementById('language2').value) : ""
-    document.getElementById('language3').checked ? languages.push(document.getElementById('language3').value) : ""
-    document.getElementById('language4').checked ? languages.push(document.getElementById('language4').value) : ""
-    document.getElementById('language5').checked ? languages.push(document.getElementById('language5').value) : ""
-
-
-
-
-
-
-
+    /* if all value have true then form will submit */
     if (!fullName_flag || !emailId_flag || !mobNum_flag || !city_flag || !gender_flag) {
+        console.log(fullName_flag, emailId_flag, mobNum_flag, city_flag, gender_flag)
         document.getElementById("submitError").innerHTML = "please enter all the required information";
         document.getElementById("submitError").style.color = "red"
     } else {
 
+        console.log("submited")
 
-
-
-        document.getElementById("contact-form").submit();
-        // document.getElementById("contact-form").reset();
-        alert("You have successfully submitted")
-
-        pc_output.innerHTML += `<tr >
-        <td>${fullName}</td>
+        /* Intert data to pc_view_table */
+        document.getElementById("user_data_pc").innerHTML += 
+        `<tr><td>${fullName}</td>
         <td>${emailId}</td>
         <td>${mobNum}</td>
         <td>${city}</td>
         <td>${male || female}</td>
-        <td>${languages}</td>
-        </tr>`
+        <td>${languages}</td></tr>`
 
-        tab_and_mob_view.innerHTML += `<tr>
+
+        /* insert data to tab_mobile_view table */
+        document.getElementById("tab_and_mob_view").innerHTML +=
+    `<tr>
         <td>Full Name</td>
-        <td>>${fullName}</td>
+        <td>${fullName}</td>
     </tr>
     <tr>
         <td>Email Id</td>
-        <td>>${emailId}</td>
+        <td>${emailId}</td>
     </tr>
     <tr>
         <td>Mobile Number</td>
@@ -198,6 +184,29 @@ function submitFrom(e) {
         <td>${languages}</td>
     </tr>`
 
+
+
+        
+        document.getElementById("contact-formm")/* .submit() */;
+
+        alert("Submitted successfully")
+        /* After submition of form make empty the input and error tags */
+        document.getElementById("contact-formm").reset();
+        document.getElementById("fullNameError").innerText = ""
+        document.getElementById("emailIdError").innerText = ""
+        document.getElementById("mobNumError").innerText = ""
+        document.getElementById("cityError").innerText = ""
+        document.getElementById("genderError").innerText = ""
+        document.getElementById("submitError").innerText = ""
+
+
+
+
+
+
+
+
+
     }
 
 
@@ -208,7 +217,7 @@ function submitFrom(e) {
 
 
 
-
+document.getElementById("sub-btn").addEventListener("click", form_handler)
 
 
 
